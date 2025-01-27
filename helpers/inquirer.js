98978b72b377f4389f269fb4f845f1c1469928c3
Emiliano Zamudio <emiliano.zamudio.alvarez@gmail.com>
2025-01-27 10:36:14 -0300

    readme added

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -14,8 +14,8 @@ require('colors');
             choices: [
                 {name: `${'1.'.green} Crear Tarea`, value: 1},
                 {name: `${'2.'.green} Listar Tareas`, value: 2},
-                {name: `${'3.'.green} Listar Tareas completadas`, value: 3},
-                {name: `${'4.'.green} Listar Tareas pendiente`, value: 4},
+                {name: `${'3.'.green} Listar Tareas Completadas`, value: 3},
+                {name: `${'4.'.green} Listar Tareas Pendientes`, value: 4},
                 {name: `${'5.'.green} Completar Tarea(s)`, value: 5},
                 {name: `${'6.'.green} Borrar Tarea`, value: 6},
                 {name: `${'0.'.green} Salir`, value: 0},
